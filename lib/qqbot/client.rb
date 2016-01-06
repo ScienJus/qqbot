dad2592aba6cf6fb7f0e37fda857d7c3b0d68868
ScienJus <xie_enlong@foxmail.com>
2016-01-06 23:21:38 +0800

    ...

--- lib/qqbot/client.rb
+++ lib/qqbot/client.rb
@@ -4,7 +4,7 @@ require 'openssl'
 module QQBot
   class Client
 
-    @@user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36';
+    @@user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
 
     def self.origin(uri)
       "#{uri.scheme}://#{uri.host}"
